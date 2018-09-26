8bc226ca5f44680d839e47f93540f60fa0a0b44e
anik ishaan <rifatislam30@gmail.com>
2018-09-26 14:17:31 +0600

    add cloud

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
-    $('.carousel.carousel-slider').carousel({
-        indicators: true
-    });
-
     $('.menu_trigger').click(function(){
         $('aside').css('transform','translateX(0)');
     });
     $('.close_trigger').click(function(){
         $('aside').css('transform','translateX(-100%)');
+    });
+    $('.scrollspy').scrollSpy();
+
+    let win = $(window);
+    win.scroll(function(){
+        if(win.scrollTop() > 255){
+            $('.go-top').fadeIn();
+        }else{
+            $('.go-top').fadeOut();
+        }
     });
 });
